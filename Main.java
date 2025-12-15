57d70158077b5f46976021ea0da93741334a7553
Admin <morbinev985@gmail.com>
2025-12-15 03:13:45 +0300

    Переменные и синтаксис

---
+++ Main.java
@@ -0,0 +1,50 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Задание 1");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        System.out.println("задание 2");
+        System.out.println(dog + 4);
+        System.out.println(cat + 4);
+        System.out.println(paper + 4);
+        System.out.println("Задаине 3");
+        System.out.println(dog - 3.5);
+        System.out.println(cat - 1.6);
+        System.out.println(paper - 7639);
+        System.out.println("Задание 4");
+        var friend = 19;
+        System.out.println(friend);
+        System.out.println(friend + 2);
+        System.out.println((friend + 2) / 7);
+        System.out.println("Задание 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        System.out.println(frog * 10);
+        System.out.println((frog * 10) / 3.5);
+        System.out.println(((frog * 10) / 3.5) + 4);
+        System.out.println("Задание 6");
+        var x = 78.2;
+        var y = 82.7;
+        System.out.println(x + y);
+        System.out.println(y - x);
+        System.out.println("Задание 7");
+        System.out.println(y / x);
+        System.out.println("Задание 8");
+        var worchours = 650;
+        var changeworkers = 8;
+        var workers = worchours / changeworkers;
+        System.out.println("всего работников в компании" + workers);
+        var c = 94;
+        var moreworkers = workers + c;
+        var moreworkhours = moreworkers * changeworkers;
+        System.out.println("если в компании работает" + moreworkers + "человек,то всего" + moreworkhours + "часов работы может быть поделено между сотрудниками");
+    }
+}
