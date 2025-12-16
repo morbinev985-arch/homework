ac4e873ca18493268452323da3c8a2c9ee4869dc
Admin <morbinev985@gmail.com>
2025-12-16 18:23:51 +0300

    Переменные и синтаксис

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Задание 1");
         var dog = 8.0;
         System.out.println(dog);
@@ -12,40 +10,51 @@ public class Main {
         var paper = 763789;
         System.out.println(paper);
         System.out.println("задание 2");
-        System.out.println(dog + 4);
-        System.out.println(cat + 4);
-        System.out.println(paper + 4);
+        dog=dog+4;
+        cat=cat+4;
+        paper=paper+4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
         System.out.println("Задаине 3");
-        System.out.println(dog - 3.5);
-        System.out.println(cat - 1.6);
-        System.out.println(paper - 7639);
+        dog=dog-3.5;
+        cat=cat-1.6;
+        paper=paper+7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
         System.out.println("Задание 4");
         var friend = 19;
         System.out.println(friend);
-        System.out.println(friend + 2);
-        System.out.println((friend + 2) / 7);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=(friend+2)/7;
+        System.out.println(friend);
         System.out.println("Задание 5");
-        var frog = 3.5;
+        var frog=3.5;
         System.out.println(frog);
-        System.out.println(frog * 10);
-        System.out.println((frog * 10) / 3.5);
-        System.out.println(((frog * 10) / 3.5) + 4);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
         System.out.println("Задание 6");
-        var x = 78.2;
-        var y = 82.7;
-        System.out.println(x + y);
-        System.out.println(y - x);
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        System.out.println(boxer1 + boxer2);
+        System.out.println(boxer2 - boxer1);
         System.out.println("Задание 7");
-        System.out.println(y / x);
+        System.out.println(boxer2 % boxer1);
         System.out.println("Задание 8");
-        var worchours = 650;
-        var changeworkers = 8;
-        var workers = worchours / changeworkers;
+        var worcHours = 650;
+        var changeWorkers = 8;
+        var workers = worcHours / changeWorkers;
         System.out.println("всего работников в компании" + workers);
         var c = 94;
-        var moreworkers = workers + c;
-        var moreworkhours = moreworkers * changeworkers;
-        System.out.println("если в компании работает" + moreworkers + "человек,то всего" + moreworkhours + "часов работы может быть поделено между сотрудниками");
+        var moreWorkers = workers + c;
+        var moreWorkhours = moreWorkers * changeWorkers;
+        System.out.println("если в компании работает" + moreWorkers + "человек,то всего" + moreWorkhours + "часов работы может быть поделено между сотрудниками");
         System.out.println("end");
 
     }
